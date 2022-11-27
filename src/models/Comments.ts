9e5fb6142a0230d75da41010ff23ce07052d7d63
adxell <adxell02@gmail.com>
2022-11-26 20:58:49 -0500

    create model

---
+++ src/models/Comments.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from 'sequelize'
+import { sequilize } from '../db'
+
+const Comments = sequilize.define(
+    "comment",
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+        },
+        comments: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        avatar: {
+            type: DataTypes.STRING,
+        },
+    }, {
+    timestamps: true
+})
+
+export const ModelComments = Comments
