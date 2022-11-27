61c536a8c8b54ac46a0a725426ad49ae751fd94d
adxell <adxell02@gmail.com>
2022-11-27 05:43:34 -0500

    new updated

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,12 @@
 import { Sequelize } from "sequelize";
-console.log(process.env.POSTGRES_DB)
+console.log(process.env.POSTGRES_PASSWORD)
 export const sequilize = new Sequelize(
     process.env.POSTGRES_DB!,
     process.env.POSTGRES_USER!,
     process.env.POSTGRES_PASSWORD!,
     {
-        host: 'localhost',
-        dialect: 'postgres'
+        host: 'containers-us-west-117.railway.app',
+        dialect: 'postgres',
+        port: 6992
     }
 )
